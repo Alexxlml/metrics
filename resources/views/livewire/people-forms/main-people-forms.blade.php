fe2350763a8695ef0429da064f76a418e18c6840
Alexx <inkisidor_117@hotmail.com>
2024-03-31 22:28:01 -0600

    Agregar badge con el total de formularios del usuario del panel, Reemplazar texto por botones de acción así como el nombre de la columna oculta por Acciones

--- resources/views/livewire/people-forms/main-people-forms.blade.php
+++ resources/views/livewire/people-forms/main-people-forms.blade.php
@@ -11,7 +11,12 @@
                 <div class="grid grid-cols-4 gap-4 px-4 sm:px-0">
                     <!-- Titulo -->
                     <div class="col-span-4 md:col-span-2 xl:col-span-3">
-                        <p class="text-xl dark:text-white text-center md:text-left">{{ $ownerUserName }}</p>
+                        <p class="text-md md:text-xl dark:text-white text-center md:text-left">{{ $ownerUserName }}
+                            <span
+                                class="bg-blue-100 text-blue-800 text-xs md:text-base font-medium me-2 px-2.5 py-0.5 rounded-full dark:bg-blue-900 dark:text-blue-300">
+                                {{ $count_forms }}
+                            </span>
+                        </p>
                     </div>
                     <!-- Botón agregar -->
                     @can('forms create')
@@ -36,77 +41,84 @@
                                 <th scope="col" class="px-6 py-3">
                                     Nombre
                                 </th>
-                                <th scope="col" class="px-6 py-3">
-                                    Clave Electoral
-                                </th>
-                                @unlessrole('Data Capturer')
-                                <th scope="col" class="px-6 py-3">
+                                <th scope="col" class="px-6 py-3 text-center">
                                     Teléfono
                                 </th>
-                                @endunlessrole
-                                @can('forms update')
-                                <th scope="col" class="px-6 py-3">
-                                    <span class="sr-only">Editar</span>
+                                <th scope="col" colspan="2" class="px-6 py-3 text-center">
+                                    Acciones
                                 </th>
-                                @endcan
-                                @hasrole('Data Capturer')
-                                <th scope="col" class="px-6 py-3">
-                                    <span class="sr-only">Solicitar edición</span>
-                                </th>
-                                <th scope="col" class="px-6 py-3">
-                                    <span class="sr-only">Ver</span>
-                                </th>
-                                @endhasrole
-                                @can('forms delete')
-                                <th scope="col" class="px-6 py-3">
-                                    <span class="sr-only">Eliminar</span>
-                                </th>
-                                @endcan
                             </tr>
                         </thead>
                         <tbody>
                             @foreach($forms as $form)
-                            <tr
-                                class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600 text-xs md:text-sm">
+                            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 text-xs md:text-sm">
                                 <th scope="row"
                                     class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
                                     {{ $form->id }}
                                 </th>
                                 <td class="px-6 py-4">
-                                    {{ $form->first_name }} {{ $form->second_name }} {{
-                                    $form->first_surname }} {{ $form->second_surname }}
+                                    {{ $form->first_name }} {{ $form->first_surname }}
                                 </td>
-                                <td class="px-6 py-4">
-                                    {{ $form->electoral_key }}
-                                </td>
-                                @unlessrole('Data Capturer')
                                 <td class="px-6 py-4">
                                     <a href="tel:{{ $form->phone }}"
-                                        class="font-medium text-blue-600 dark:text-blue-500 hover:underline">{{
-                                        $form->phone }}</a>
+                                        class="flex justify-center text-green-600 hover:text-green-700 dark:text-green-500 hover:dark:text-green-600"><svg
+                                            xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"
+                                            class="w-6 h-6">
+                                            <path fill-rule="evenodd"
+                                                d="M15 3.75a.75.75 0 0 1 .75-.75h4.5a.75.75 0 0 1 .75.75v4.5a.75.75 0 0 1-1.5 0V5.56l-4.72 4.72a.75.75 0 1 1-1.06-1.06l4.72-4.72h-2.69a.75.75 0 0 1-.75-.75Z"
+                                                clip-rule="evenodd" />
+                                            <path fill-rule="evenodd"
+                                                d="M1.5 4.5a3 3 0 0 1 3-3h1.372c.86 0 1.61.586 1.819 1.42l1.105 4.423a1.875 1.875 0 0 1-.694 1.955l-1.293.97c-.135.101-.164.249-.126.352a11.285 11.285 0 0 0 6.697 6.697c.103.038.25.009.352-.126l.97-1.293a1.875 1.875 0 0 1 1.955-.694l4.423 1.105c.834.209 1.42.959 1.42 1.82V19.5a3 3 0 0 1-3 3h-2.25C8.552 22.5 1.5 15.448 1.5 6.75V4.5Z"
+                                                clip-rule="evenodd" />
+                                        </svg>
+                                    </a>
+                                </td>
+                                @unlessrole('Data Capturer')
+                                <td class="py-4">
+                                    <a href="{{ route('editar-formulario', ['id' => $form->id]) }}"
+                                        class="flex justify-center text-yellow-600 hover:text-yellow-700 dark:text-yellow-500 hover:dark:text-yellow-600">
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                            stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                            <path stroke-linecap="round" stroke-linejoin="round"
+                                                d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
+                                        </svg>
+                                    </a>
                                 </td>
                                 @endunlessrole
-                                @can('forms update')
-                                <td class="px-6 py-4 text-right">
-                                    <a href="{{ route('editar-formulario', ['id' => $form->id]) }}"
-                                        class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Editar</a>
-                                </td>
-                                @endcan
                                 @hasrole('Data Capturer')
-                                <td class="px-6 py-4 text-right">
-                                    <button type="button" wire:click="openModalToChange({{ $form->id }})"
-                                        class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Solicitar
-                                        edición</button>
+                                <td class="py-4">
+                                    <a role="button" tabindex="0" wire:click="openModalToChange({{ $form->id }})"
+                                        class="flex justify-center text-yellow-600 hover:text-yellow-700 dark:text-yellow-500 hover:dark:text-yellow-600">
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                            stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                            <path stroke-linecap="round" stroke-linejoin="round"
+                                                d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
+                                        </svg>
+                                    </a>
                                 </td>
-                                <td class="px-6 py-4 text-right">
+                                <td class="py-4">
                                     <a href="{{ route('editar-formulario', ['id' => $form->id]) }}"
-                                        class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Ver</a>
+                                        class="flex justify-center text-blue-600 hover:text-blue-700 dark:text-blue-500 hover:dark:text-blue-600">
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                            stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                            <path stroke-linecap="round" stroke-linejoin="round"
+                                                d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z" />
+                                            <path stroke-linecap="round" stroke-linejoin="round"
+                                                d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+                                        </svg>
+                                    </a>
                                 </td>
                                 @endhasrole
                                 @can('forms delete')
-                                <td class="px-6 py-4 text-right">
-                                    <button type="button" wire:click="deleteForm({{ $form->id }})"
-                                        class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Eliminar</button>
+                                <td class="py-4">
+                                    <a wire:click="deleteForm({{ $form->id }})"
+                                        class="flex justify-center text-red-600 hover:text-red-700 dark:text-red-500 hover:dark:text-red-600">
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                            stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                            <path stroke-linecap="round" stroke-linejoin="round"
+                                                d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
+                                        </svg>
+                                    </a>
                                 </td>
                                 @endcan
                             </tr>
