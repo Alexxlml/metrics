bf27fef537db5ca73ba9908a4587bfc44e58789b
Alexx <inkisidor_117@hotmail.com>
2024-04-07 23:12:52 -0600

    Agregar campo Voto en vista Livewire main-people-forms: - Se agregó la columna Voto a la tabla de Formularios así como un input checkbox para llamar a la función changeVoteState.

--- resources/views/livewire/people-forms/main-people-forms.blade.php
+++ resources/views/livewire/people-forms/main-people-forms.blade.php
@@ -41,6 +41,9 @@
                                 <th scope="col" class="px-6 py-3">
                                     Nombre
                                 </th>
+                                <th scope="col" class="px-6 py-3">
+                                    Voto
+                                </th>
                                 <th scope="col" class="px-6 py-3 text-center">
                                     Teléfono
                                 </th>
@@ -58,6 +61,14 @@
                                 </th>
                                 <td class="px-6 py-4">
                                     {{ $form->first_name }} {{ $form->first_surname }}
+                                </td>
+                                <td class="px-6 py-4">
+                                    <label class="inline-flex items-center cursor-pointer">
+                                        <input type="checkbox" class="sr-only peer" wire:click="changeVoteState({{ $form->id }}, {{ $form->vote }})" @if($form->vote) checked @endif>
+                                        <div
+                                            class="relative w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600">
+                                        </div>
+                                    </label>
                                 </td>
                                 <td class="px-6 py-4">
                                     <a href="tel:{{ $form->phone }}"
