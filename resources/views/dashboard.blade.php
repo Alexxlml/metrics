58676c498a34a5a61b16f249fb238d0b827a7166
Alexx <inkisidor_117@hotmail.com>
2024-04-09 01:35:53 -0600

    Modificar vista Dashboard: - Se añadió un nuevo bloque al dashboard para dar la bienvenida al usuario.

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -8,7 +8,12 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-xl sm:rounded-lg">
-                <x-welcome />
+                <div class="flex flex-col space-y-2 p-4 text-center">
+                    <h1 class="dark:text-gray-200 text-xl md:text-2xl">
+                        Usa la barra de navegación para acceder las herramientas de este sistema.
+                    </h1>
+                    <p class="dark:text-gray-200 md:text-xl">Si tienes dudas contacta con tu superior.</p>
+                </div>
             </div>
         </div>
     </div>
