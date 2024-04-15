046e686d0550e0df9cda7e3f6c230e2668ec10f8
Alexx <inkisidor_117@hotmail.com>
2024-04-15 02:42:40 -0600

    Corrección en componente ChartPerHour: - Se corrigieron las fechas de la consulta de votos para el día 2 de junio del 2024

--- app/Livewire/Reports/ChartPerHour.php
+++ app/Livewire/Reports/ChartPerHour.php
@@ -21,78 +21,78 @@ class ChartPerHour extends Component
         array_push($this->votes, array(
             'hora' => '8:00',
             'total' => PeopleForm::where('vote', 1)
-                ->where('updated_at', '>=', '2024-04-14 8:00')
-                ->where('updated_at', '<', '2024-04-14 9:00')
+                ->where('updated_at', '>=', '2024-06-02 8:00')
+                ->where('updated_at', '<', '2024-06-02 9:00')
                 ->count()
         ));
         array_push($this->votes, array(
             'hora' => '9:00',
             'total' => PeopleForm::where('vote', 1)
-                ->where('updated_at', '>=', '2024-04-14 9:00')
-                ->where('updated_at', '<', '2024-04-14 10:00')
+                ->where('updated_at', '>=', '2024-06-02 9:00')
+                ->where('updated_at', '<', '2024-06-02 10:00')
                 ->count()
         ));
         array_push($this->votes, array(
             'hora' => '10:00',
             'total' => PeopleForm::where('vote', 1)
-                ->where('updated_at', '>=', '2024-04-14 10:00')
-                ->where('updated_at', '<', '2024-04-14 11:00')
+                ->where('updated_at', '>=', '2024-06-02 10:00')
+                ->where('updated_at', '<', '2024-06-02 11:00')
                 ->count()
         ));
         array_push($this->votes, array(
             'hora' => '11:00',
             'total' => PeopleForm::where('vote', 1)
-                ->where('updated_at', '>=', '2024-04-14 11:00')
-                ->where('updated_at', '<', '2024-04-14 12:00')
+                ->where('updated_at', '>=', '2024-06-02 11:00')
+                ->where('updated_at', '<', '2024-06-02 12:00')
                 ->count()
         ));
         array_push($this->votes, array(
             'hora' => '12:00',
             'total' => PeopleForm::where('vote', 1)
-                ->where('updated_at', '>=', '2024-04-14 12:00')
-                ->where('updated_at', '<', '2024-04-14 13:00')
+                ->where('updated_at', '>=', '2024-06-02 12:00')
+                ->where('updated_at', '<', '2024-06-02 13:00')
                 ->count()
         ));
         array_push($this->votes, array(
             'hora' => '13:00',
             'total' => PeopleForm::where('vote', 1)
-                ->where('updated_at', '>=', '2024-04-14 13:00')
-                ->where('updated_at', '<', '2024-04-14 14:00')
+                ->where('updated_at', '>=', '2024-06-02 13:00')
+                ->where('updated_at', '<', '2024-06-02 14:00')
                 ->count()
         ));
         array_push($this->votes, array(
             'hora' => '14:00',
             'total' => PeopleForm::where('vote', 1)
-                ->where('updated_at', '>=', '2024-04-14 14:00')
-                ->where('updated_at', '<', '2024-04-14 15:00')
+                ->where('updated_at', '>=', '2024-06-02 14:00')
+                ->where('updated_at', '<', '2024-06-02 15:00')
                 ->count()
         ));
         array_push($this->votes, array(
             'hora' => '15:00',
             'total' => PeopleForm::where('vote', 1)
-                ->where('updated_at', '>=', '2024-04-14 15:00')
-                ->where('updated_at', '<', '2024-04-14 16:00')
+                ->where('updated_at', '>=', '2024-06-02 15:00')
+                ->where('updated_at', '<', '2024-06-02 16:00')
                 ->count()
         ));
         array_push($this->votes, array(
             'hora' => '16:00',
             'total' => PeopleForm::where('vote', 1)
-                ->where('updated_at', '>=', '2024-04-14 16:00')
-                ->where('updated_at', '<', '2024-04-14 17:00')
+                ->where('updated_at', '>=', '2024-06-02 16:00')
+                ->where('updated_at', '<', '2024-06-02 17:00')
                 ->count()
         ));
         array_push($this->votes, array(
             'hora' => '17:00',
             'total' => PeopleForm::where('vote', 1)
-                ->where('updated_at', '>=', '2024-04-14 17:00')
-                ->where('updated_at', '<', '2024-04-14 18:00')
+                ->where('updated_at', '>=', '2024-06-02 17:00')
+                ->where('updated_at', '<', '2024-06-02 18:00')
                 ->count()
         ));
         array_push($this->votes, array(
             'hora' => '18:00',
             'total' => PeopleForm::where('vote', 1)
-                ->where('updated_at', '>=', '2024-04-14 18:00')
-                ->where('updated_at', '<', '2024-04-14 19:00')
+                ->where('updated_at', '>=', '2024-06-02 18:00')
+                ->where('updated_at', '<', '2024-06-02 19:00')
                 ->count()
         ));
 
